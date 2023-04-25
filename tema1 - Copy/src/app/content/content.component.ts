6a0659cdfeab7f0a95248a04be3ed370eab15d73
Radu2703 <serbanescuradu254@gmail.com>
2023-04-25 20:56:15 +0300

    tema1-copy

---
+++ tema1 - Copy/src/app/content/content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss']
+})
+export class ContentComponent {
+
+}
+//@MatCardModule({selector: 'mat-card'})
+//export class MatCardModule{matCard}
